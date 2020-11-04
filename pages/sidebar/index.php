28aceb1157c49cf01fcf8cbbaec40ba6b2502cc9
Fxluan <f.basroh7@gmail.com>
2020-11-04 13:01:51 +0700

    update layout

---
+++ pages/sidebar/index.php
@@ -0,0 +1,30 @@
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h3 class="card-title">PROFESSIONAL DETAILS</h3>
+      <!-- <p class="card-text"></p> -->
+      <table>
+          <tr>
+              <td>
+                  <h4>NAME</h4>
+              </td>
+              <td>
+                  :
+              </td>
+              <td>
+                  Fachruddin Basroh
+              </td>
+          </tr>
+          <tr>
+            <td>
+                <h4>NAME</h4>
+            </td>
+            <td>
+                :
+            </td>
+            <td>
+                Fachruddin Basroh
+            </td>
+        </tr>
+      </table>
+    </div>
+  </div>
